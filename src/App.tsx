a3f761491a1f73726577abed24524cc2246ed91e
webLXM <1944754630@qq.com>
2024-08-25 21:29:10 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useRoutes } from 'react-router-dom'
+import routes from '@/router'
+
+function App() {
+  const ElementRouter = useRoutes(routes)
+  return (
+    <div>
+      { ElementRouter }
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'node:path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const envConfig = loadEnv(mode, './')
+
+  return {
+    plugins: [react()],
+    // 配置路径别名
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: Number(envConfig.VITE_PORT) || 3000,
+      proxy: {
+        '/api': {
+          target: envConfig.VITE_API_BASEURL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
+})
