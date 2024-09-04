36513276cfc7f56968569a232a3c8ae44fc9e54f
webLXM <1944754630@qq.com>
2024-09-04 23:49:09 +0800

    feat(star): 星标问卷界面

--- src/views/manage/list/index.tsx
+++ src/views/manage/list/index.tsx
@@ -1,21 +1,25 @@
 import type { FC } from 'react'
 import { useState } from 'react'
-import { Typography } from 'antd'
+import { Empty, Typography } from 'antd'
+import { useTitle } from 'ahooks'
 import styles from '../common.module.scss'
 import type { PropsType } from '@/components/QuestionCard'
 import QuestionCard from '@/components/QuestionCard'
 
 const { Title } = Typography
 
+const rawQuestionList = [{
+  _id: '1', // 服务端 mongodb ，自动，_id 不重复
+  title: '问卷1',
+  isStar: true,
+  isPublished: true,
+  answerCount: 100,
+  createdAt: '小生'
+}]
+
 const List: FC = () => {
-  const [questionList] = useState([{
-    _id: '1', // 服务端 mongodb ，自动，_id 不重复
-    title: '问卷1',
-    isStar: true,
-    isPublished: true,
-    answerCount: 100,
-    createdAt: '小生'
-  }]) // 全部的列表数据，上划加载更多，累计
+  useTitle('小星问卷 - 我的问卷')
+  const [questionList] = useState(rawQuestionList) // 全部的列表数据，上划加载更多，累计
   return (
     <>
       <div className={styles.header}>
@@ -28,6 +32,7 @@ const List: FC = () => {
       </div>
       <div className={styles.content}>
         {/* 问卷列表 */}
+        {questionList.length === 0 && <Empty description="暂无数据" />}
         {
           questionList.length > 0
           && questionList.map((q: PropsType) => {
--- src/views/manage/star/index.tsx
+++ src/views/manage/star/index.tsx
@@ -1,8 +1,47 @@
 import type { FC } from 'react'
+import { useState } from 'react'
+import { Empty, Typography } from 'antd'
+import styles from '@/views/manage/common.module.scss'
+import type { PropsType } from '@/components/QuestionCard'
+import QuestionCard from '@/components/QuestionCard'
+import {useTitle} from "ahooks";
 
+const { Title } = Typography
+
+const rawQuestionList = [{
+  _id: '1', // 服务端 mongodb ，自动，_id 不重复
+  title: '问卷1',
+  isStar: true,
+  isPublished: true,
+  answerCount: 100,
+  createdAt: '小生'
+}]
 const Star: FC = () => {
+  useTitle('小星问卷 - 星标问卷')
+  const [questionList] = useState(rawQuestionList)
   return (
-    <div>星标问卷</div>
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>星标问卷</Title>
+        </div>
+        <div className={styles.right}>
+          搜索
+        </div>
+      </div>
+      <div className={styles.content}>
+        {/* 问卷列表 */}
+        {questionList.length === 0 && <Empty description="暂无数据" />}
+        {
+          questionList.length > 0
+          && questionList.map((q: PropsType) => {
+            const { _id } = q
+            return <QuestionCard key={_id} {...q} />
+          })
+        }
+      </div>
+      <div className={styles.footer}>分页</div>
+    </>
   )
 }
 export default Star
