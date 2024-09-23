06cb5140d589d6ebcd5219dcbc61fe49caf912cd
webLXM <1944754630@qq.com>
2024-09-23 23:10:13 +0800

    feat(edit): 编辑问卷页面布局样式

--- src/views/question/edit/index.tsx
+++ src/views/question/edit/index.tsx
@@ -1,12 +1,19 @@
 import type { FC } from 'react'
-import { useParams } from 'react-router-dom'
+import styles from './index.module.scss'
 
 const Edit: FC = () => {
-  const { id } = useParams()
   return (
-    <div>
-      编辑问卷
-      <h2>{ id }</h2>
+    <div className={styles.container}>
+      <div className="h-10 bg-#fff">Header</div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}>main</div>
+          </div>
+          <div className={styles.right}>right</div>
+        </div>
+      </div>
     </div>
   )
 }
