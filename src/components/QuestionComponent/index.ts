a8919e0cf94ae434c4456dd91894f6b0544bd8dc
webLXM <1944754630@qq.com>
2024-09-26 23:13:51 +0800

    feat(LeftPanel): 组件库分组显示

--- src/components/QuestionComponent/index.ts
+++ src/components/QuestionComponent/index.ts
@@ -5,10 +5,10 @@ import QuestionTitleConfig from './QuestionTitle'
 import QuestionInputConfig from './QuestionInput'
 
 // 各个组件的 prop  type
-export type ComponentPropsType = IQuestionInputProps & IQuestionTitleProps
+type ComponentPropsType = IQuestionInputProps & IQuestionTitleProps
 
 // 组件的配置
-export interface IComponentConfig {
+interface IComponentConfig {
   title: string
   type: string
   Component: FC<ComponentPropsType>
@@ -18,6 +18,30 @@ export interface IComponentConfig {
 //  全部组件配置的列表
 const componentConfigList: IComponentConfig[] = [QuestionTitleConfig, QuestionInputConfig]
 
-export function getComponentConfigByType(type: string) {
+// 组件分组
+const componentConfigGroup = [
+  {
+    groupId: 'textGroup',
+    groupName: '文本显示',
+    conponents: [QuestionTitleConfig]
+  },
+  {
+    groupId: 'inputGroup',
+    groupName: '用户输入',
+    conponents: [QuestionInputConfig]
+  }
+]
+
+function getComponentConfigByType(type: string) {
   return componentConfigList.find(item => item.type === type)
+}
+
+export type {
+  ComponentPropsType,
+  IComponentConfig
+}
+
+export {
+  componentConfigGroup,
+  getComponentConfigByType
 }
--- src/views/question/edit/LeftPanel/ComponentLib.tsx
+++ src/views/question/edit/LeftPanel/ComponentLib.tsx
@@ -1,7 +1,36 @@
 import type { FC } from 'react'
+import { Typography } from 'antd'
+import styles from './ComponentLib.module.scss'
+import type { IComponentConfig } from '@/components/QuestionComponent'
+import { componentConfigGroup } from '@/components/QuestionComponent'
+
+const { Title } = Typography
+
+function genComponent(c: IComponentConfig) {
+  const { Component } = c
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.component}>
+        <Component />
+      </div>
+    </div>
+  )
+}
 
 const ComponentLib: FC = () => {
-  return <div>组件库</div>
+  return (
+    <>
+      {componentConfigGroup.map((group) => {
+        const { groupId, groupName, conponents } = group
+        return (
+          <div key={groupId}>
+            <Title level={5}>{groupName}</Title>
+            <div>{conponents.map(c => genComponent(c))}</div>
+          </div>
+        )
+      })}
+    </>
+  )
 }
 
 export default ComponentLib
