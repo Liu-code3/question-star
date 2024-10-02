05eac5be87c0a279cf965f6f05c99f2a06f74deb
webLXM <1944754630@qq.com>
2024-10-02 22:07:37 +0800

    feat(RightPanel): 左侧面板增加页面设置模块—抽离组件

---
+++ src/views/question/edit/RightPanel/Setting.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react'
+
+const Setting: FC = () => {
+    return <div>setting</div>
+}
+
+export default Setting
--- src/views/question/edit/RightPanel/index.tsx
+++ src/views/question/edit/RightPanel/index.tsx
@@ -1,25 +1,40 @@
-import type { FC } from 'react'
+import {FC, useEffect, useState} from 'react'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import { Tabs } from 'antd'
 import ComponentProp from '@/views/question/edit/RightPanel/ComponentProp.tsx'
+import Setting from '@/views/question/edit/RightPanel/Setting.tsx'
+import { useGetComponentInfo } from '@/hooks/useGetComponentInfo.ts'
 
+enum TAB_KEYS  {
+  PROP_KEY = 'prop',
+  SETTING_KEY = 'setting'
+}
 const RightPanel: FC = () => {
+  const { selectedId } = useGetComponentInfo()
+
+  const [activeKey, setActiveKey] = useState(TAB_KEYS.PROP_KEY)
+
+  useEffect(() => {
+    const key = selectedId ? TAB_KEYS.PROP_KEY : TAB_KEYS.SETTING_KEY
+    setActiveKey(key)
+  }, [selectedId]);
+
   const tabItems = [
     {
-      key: 'prop',
+      key: TAB_KEYS.PROP_KEY,
       label: '属性',
       icon: <FileTextOutlined />,
       children: <ComponentProp />
     },
     {
-      key: 'setting',
+      key: TAB_KEYS.SETTING_KEY,
       label: '页面设置',
       icon: <SettingOutlined />,
-      children: <div>页面设置</div>
+      children: <Setting />
     }
   ]
 
-  return <Tabs items={tabItems} defaultActiveKey="prop"></Tabs>
+  return <Tabs items={tabItems} activeKey={activeKey}></Tabs>
 }
 
 export default RightPanel
